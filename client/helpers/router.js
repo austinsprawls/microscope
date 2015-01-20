96f60d712465147eb5ca0875bb00d036335d6aab
Austin Sprawls <austinsprawls@gmail.com>
2015-01-20 14:08:28 -0600

    Routing to a single post page

--- client/helpers/router.js
+++ client/helpers/router.js
@@ -7,3 +7,8 @@ Router.configure({
 Router.route('/', function() {
   this.render('postsList');
 });
+
+Router.route('/posts/:_id', function() {
+  var post = Posts.findOne({_id: this.params._id});
+  this.render('postPage', {data: post});
+});
